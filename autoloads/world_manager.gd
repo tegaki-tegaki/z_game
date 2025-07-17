7204b6648aad3d8b66d52a89aecc9ca2cfed25ab
tegaki <drawydraws@protonmail.com>
2025-07-18 01:07:18 +0200

    init commit

---
+++ autoloads/world_manager.gd
@@ -0,0 +1,68 @@
+extends Node
+const ENEMY = preload("res://enemy.tscn")
+const MAIN = preload("res://main.tscn")
+
+static var images_data = {
+    "small.png": {"texture": preload("res://resources/small.png"), "hframes": 16, "vframes": 110},
+    "normal.png": {"texture": preload("res://resources/normal.png"), "hframes": 16, "vframes": 445},
+    "tall.png": {"texture": preload("res://resources/tall.png"), "hframes": 16, "vframes": 44},
+    "human_body.png":
+    {"texture": preload("res://resources/human_body.png"), "hframes": 16, "vframes": 167},
+    "human_body_plus.png":
+    {"texture": preload("res://resources/human_body_plus.png"), "hframes": 16, "vframes": 38},
+    "centered.png":
+    {"texture": preload("res://resources/centered.png"), "hframes": 16, "vframes": 3},
+    "large.png": {"texture": preload("res://resources/large.png"), "hframes": 16, "vframes": 34},
+    "large_ridden.png":
+    {"texture": preload("res://resources/large_ridden.png"), "hframes": 3, "vframes": 2},
+    "huge.png": {"texture": preload("res://resources/huge.png"), "hframes": 4, "vframes": 8},
+    "giant.png": {"texture": preload("res://resources/giant.png"), "hframes": 4, "vframes": 22},
+    "incomplete_small.png":
+    {"texture": preload("res://resources/incomplete_small.png"), "hframes": 16, "vframes": 1},
+    "incomplete.png":
+    {"texture": preload("res://resources/incomplete.png"), "hframes": 16, "vframes": 54},
+    "incomplete_tall.png":
+    {"texture": preload("res://resources/incomplete_tall.png"), "hframes": 16, "vframes": 1},
+    "incomplete_body_plus.png":
+    {"texture": preload("res://resources/incomplete_body_plus.png"), "hframes": 16, "vframes": 1},
+    "incomplete_large.png":
+    {"texture": preload("res://resources/incomplete_large.png"), "hframes": 8, "vframes": 11},
+    "incomplete_giant.png":
+    {"texture": preload("res://resources/incomplete_giant.png"), "hframes": 4, "vframes": 1},
+    "fillerhoder.png":
+    {"texture": preload("res://resources/fillerhoder.png"), "hframes": 16, "vframes": 4},
+    "filler.png": {"texture": preload("res://resources/filler.png"), "hframes": 16, "vframes": 2},
+    "filler_tall.png":
+    {"texture": preload("res://resources/filler_tall.png"), "hframes": 1, "vframes": 1},
+    "fillergiant.png":
+    {"texture": preload("res://resources/fillergiant.png"), "hframes": 4, "vframes": 3},
+    "fallback.png":
+    {"texture": preload("res://resources/fallback.png"), "hframes": 16, "vframes": 256}
+}
+
+
+func _ready():
+    var config = Utils.load_json_config()
+    var monsters = Utils.process_config(config)
+
+    for i in range(0, 50):
+        var enemy = ENEMY.instantiate()
+        var enemy_data = monsters.pick_random()
+
+        var sprite = enemy.get_node("Sprite2D") as Sprite2D
+        var label = enemy.get_node("name") as Label
+
+        var image_data = images_data[enemy_data.file_name]
+        sprite.texture = image_data.texture
+        sprite.hframes = image_data.hframes
+        sprite.vframes = image_data.vframes
+        sprite.frame = enemy_data.id
+
+        if typeof(enemy_data.name) == TYPE_ARRAY:
+          label.text = enemy_data.name[0]
+        else:
+          label.text = enemy_data.name
+
+        enemy.position = Vector2(i % 10 * 300 + 100, (i / 10) * 100 + 100)
+
+        get_tree().current_scene.add_child(enemy)
---
+++ scripts/Utils.gd
@@ -0,0 +1,84 @@
+class_name Utils
+
+# the fg (and bg) ids refer to the "global" id,
+# to be interpreted as if all tilesets were stitched together
+# subtract one of these values to get a "local" id within a tileset
+static var image_tile_offsets = {
+    "small.png": 0,
+    "normal.png": 1760,
+    "tall.png": 8880,
+    "human_body.png": 9584,
+    "human_body_plus.png": 12256,
+    "centered.png": 12864,
+    "large.png": 12900,
+    "large_ridden.png": 13172,
+    "huge.png": 13178,
+    "giant.png": 13210,
+    "incomplete_small.png": 13298,
+    "incomplete.png": 13314,
+    "incomplete_tall.png": 14178,
+    "incomplete_body_plus.png": 14194,
+    "incomplete_large.png": 14210,
+    "incomplete_giant.png": 14298,
+    "fillerhoder.png": 14302,
+    "filler.png": 14366,
+    "filler_tall.png": 14398,
+    "fillergiant.png": 14400,
+    # "fallback.png": 14412
+}
+
+
+static func get_image_id(image_tile, global_image_id):
+    return global_image_id - image_tile_offsets[image_tile]
+
+
+static func find_tile_data(tiles, target: String) -> Variant:
+    for tile in tiles:
+        if typeof(tile.id) == TYPE_STRING && tile.id == target:
+            return tile
+        if typeof(tile.id) == TYPE_ARRAY:
+            for id in tile.id:
+                if id == target:
+                    return tile
+    return null
+
+
+# target string is a prefix
+static func filter_tile_data(tiles, target: String) -> Variant:
+    var filtered = []
+    for tile in tiles:
+        if typeof(tile.id) == TYPE_STRING && tile.id.begins_with(target):
+            filtered.append(tile)
+        if typeof(tile.id) == TYPE_ARRAY:
+            for id in tile.id:
+                if id.begins_with(target):
+                    filtered.append(tile)
+    return filtered
+
+
+# TODO: i don't like higher order functions...
+static func process_monsters(file):
+    return func(monster):
+        if typeof(monster) != TYPE_DICTIONARY || typeof(monster.fg) != TYPE_FLOAT:
+            # NOTE: probably a better way? (some have multiple ids map to one fg + vice versa)
+            return null
+        return {"file_name": file, "name": monster.id, "id": get_image_id(file, monster.fg)}
+
+
+static func process_config(config):
+    var monsters = []
+    for tile_data in config["tiles-new"]:
+        var tiles = tile_data.tiles
+        var monsters_chunk_raw = filter_tile_data(tiles, "mon_zombie")
+        var monsters_chunk = monsters_chunk_raw.map(process_monsters(tile_data.file))
+        monsters.append_array(monsters_chunk)
+    monsters = monsters.filter(func(m): return m != null)
+    return monsters
+
+
+static func load_json_config():
+    var file_json = FileAccess.get_file_as_string("res://resources/tile_config.json")
+    var config = JSON.parse_string(file_json)
+
+    if config:
+        return config
